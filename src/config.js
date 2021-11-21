572d33669c1d4cd0d75b2969bd6d9bf3457f7c6c
dromaz <92167972+dromaz@users.noreply.github.com>
2021-11-21 18:08:02 +0100

    stupid boy don't let 10k on github

--- src/config.js
+++
@@ -1,8 +0,0 @@
-// Discord Config
-export const TOKEN = "ODg0OTA5MTczMzM3MzY2NTI5.YYKQMw.U6QEKw2A0KEUnBQKe8PAFT8949w";
-
-// Ftx Config
-export const FTX_SECRET= "a8r8qv5dZv2gPFyb-iqmDG72I74q5eQdsX8OOOmo"; 
-export const FTX_API = "FxE0mIjYXtCEzYfzlTGmenOi66LutOMq-Ivb1ps7";
-export const SUBACCOUNT = "MAINBOT";
-export const RISK = 0.02;
